7549a00d904c84da1251c9f9f6888d77630c591f
xxxxfault <jiaqian0730@gmail.com>
2022-10-18 15:20:40 -0400

    test eb

---
+++ server.ts
@@ -0,0 +1,43 @@
+/**
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ * </ul>
+ * 
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
+ */
+import express, {Request, Response} from 'express';
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+import LikeController from "./controllers/LikeController";
+import mongoose from "mongoose";
+var cors = require('cors')
+
+// build the connection string
+mongoose.connect('mongodb+srv://jiaqian:Xujiaqiano@cluster0.ucuihp7.mongodb.net/?retryWrites=true&w=majority')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
+
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+
+// create RESTful Web service API
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+const likesController = LikeController.getInstance(app);
+
+/**
+ * Start a server listening at port 4000 locally
+ * but use environment variable PORT on Heroku if available.
+ */
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
